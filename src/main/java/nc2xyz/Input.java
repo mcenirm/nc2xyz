c5d7f479eb605446a4fd741e1c6451dab97c786c
Michael McEniry (UAH) <mcenirm@uah.edu>
2013-06-03 15:39:06 -0500

    require variable name

--- src/main/java/nc2xyz/Input.java
+++ src/main/java/nc2xyz/Input.java
@@ -35,10 +35,6 @@ public class Input {
         this.coordinateArrays = new Array[coordinateVariables.length];
     }
 
-    Input(String string) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
     public NetcdfFile getNetcdfFile() {
         return netcdfFile;
     }
--- src/main/java/nc2xyz/Main.java
+++ src/main/java/nc2xyz/Main.java
@@ -11,10 +11,10 @@ public class Main
     public static void main( String[] args ) throws IOException
     {
         if (args.length != 1) {
-            System.err.println(String.format("Usage: %s NC_FILE", Main.class.getName()));
+            System.err.println(String.format("Usage: %s NC_FILE VAR_NAME", Main.class.getName()));
             System.exit(1);
         }
-        Input input = new Input(args[0]);
+        Input input = new Input(args[0], args[1]);
         Converter converter = new Converter(input);
         converter.convert();
     }
